1b73a1f8f278c75a34dd431b87c79c2698554432
Arleen Amala <arleenamala@gmail.com>
2012-10-27 13:19:54 -0700

    Logout functionality

--- public/js/blogController.js
+++ public/js/blogController.js
@@ -1,16 +1,57 @@
 var BlogController = function() {
-	this.showLoginPage = function() {
+	
+	this.urls = {
+		'login': 'user_details/login.json'
+	};
+	
+	this.globalGetRequestOptions = {
+		type: 'GET',
+		contentType: 'application/json'
+	};
+	
+	this.showLoginScreen = function() {
+		//clear the username and password values
+		$("#username").attr('value', '');
+		$("#password").attr('value', '');
+		
+    	$("#loginContainer").css("display", "");
+		$("#username").focus();
+    };
+    
+    this.login = function() {
+    	var username = $("#username").val();
+    	var password = $("#password").val();
+		$.post(this.urls['login'], {"name": username, "password": password}, this.loginResponseHandler, "json");
+    };
+    
+    this.loginResponseHandler = function(data) {
+    	var loginResult = data.hashedname;
+    	if(loginResult) {
+    		//Valid result, login success, show the blogs list page
+    		blogController.getBlogs();
+    	} else {
+    		//TODO: Report incorrect login 
+    	}
+    }
+    this.logout = function() {
+    	//hide all existing containers
+    	$(".container").css("display", "none");
     	
+    	//reset the blogController state
+    	
+    	//Clear the login page details and show it
+    	this.showLoginScreen();
     };
     
     this.getBlogs = function() {
       var url = '/blogs/jsonview.json';
-      var options = {type: 'GET', contentType: "application/json"};      
-      $.ajax(url, options).done(function(data) {
+      $.ajax(url, this.globalGetRequestOptions).done(function(data) {
       	
       	//Hide all existing containers
+      	$(".container").css("display", "none");
       	
       	//Show the Blog list container with the buttons
+      	$("#blogsContainer").css("display", "");
       	
       	//setup the blog list
       	$("#blogEntries").empty();
